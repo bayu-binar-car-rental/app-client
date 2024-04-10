ba76f088a890bc1fe2c94c5678044e58ed3c3fc8
TamaWira <asusrog20012001@gmail.com>
2024-04-10 19:06:27 +0800

    feat: update Car Filter

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -2,16 +2,19 @@ import { PropsWithChildren } from "react";
 
 interface IProps {
   variant?: "primary" | "secondary";
-  onclick: () => void;
+  type?: "submit" | "reset" | "button" | undefined;
+  onclick?: () => void;
 }
 
 export default function Button({
   children,
   variant = "primary",
+  type,
   onclick,
 }: PropsWithChildren<IProps>) {
   return (
     <button
+      type={type}
       onClick={onclick}
       className={`p-1 px-5 font-bold ${
         variant === "primary"
---
+++ src/utils/MyDate.ts
@@ -0,0 +1,17 @@
+export default class MyDate {
+  constructor() {}
+
+  today() {
+    const date = new Date();
+    const year = date.getFullYear();
+
+    let month = (date.getMonth() + 1).toString();
+    if (month.length === 1) {
+      month = "0" + month;
+    }
+
+    const day = date.getDate();
+
+    return year + "-" + month + "-" + day;
+  }
+}
