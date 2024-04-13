4bcff161b6e296cb035cef054d3e85f2101cdada
TamaWira <asusrog20012001@gmail.com>
2024-04-13 19:48:29 +0800

    feat: update payment deadline countdown

--- src/hooks/useTimer.ts
+++ src/hooks/useTimer.ts
@@ -1,17 +1,34 @@
 import { useEffect, useState } from "react";
 
 export default function useTimer(deadline: string) {
-  const [hours, setHours] = useState<number>(0);
-  const [minutes, setMinutes] = useState<number>(0);
-  const [seconds, setSeconds] = useState<number>(0);
+  const [hours, setHours] = useState<string>("00");
+  const [minutes, setMinutes] = useState<string>("00");
+  const [seconds, setSeconds] = useState<string>("00");
 
   const getTime = (deadline: string) => {
     console.log(deadline);
     const time = Date.parse(deadline) - Date.now();
 
-    setHours(Math.floor((time / (1000 * 60 * 60)) % 24));
-    setMinutes(Math.floor((time / 1000 / 60) % 60));
-    setSeconds(Math.floor((time / 1000) % 60));
+    const calculateHour = Math.floor((time / (1000 * 60 * 60)) % 24).toString();
+    if (calculateHour.length === 1) {
+      setHours("0" + calculateHour);
+    } else {
+      setHours(calculateHour);
+    }
+
+    const calculateMinute = Math.floor((time / 1000 / 60) % 60).toString();
+    if (calculateMinute.length === 1) {
+      setMinutes("0" + calculateMinute);
+    } else {
+      setMinutes(calculateMinute);
+    }
+
+    const calculateSecond = Math.floor((time / 1000) % 60).toString();
+    if (calculateSecond.length === 1) {
+      setSeconds("0" + calculateSecond);
+    } else {
+      setSeconds(calculateSecond);
+    }
   };
 
   useEffect(() => {
