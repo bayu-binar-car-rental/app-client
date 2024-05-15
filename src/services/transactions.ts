b46380c36a9fc62e8676cb03cb597883f34451ff
TamaWira <asusrog20012001@gmail.com>
2024-05-15 17:32:07 +0800

    feat: add transactions service

---
+++ src/services/transactions.ts
@@ -0,0 +1,53 @@
+import { ITransactions } from "../types/transaction";
+const BASE_URL = "https://binar-car-rental-api-bayu.fly.dev/api/v1";
+
+// Fetch Transaction List
+export const fetchTransactions = async () => {
+  try {
+    const response = await fetch(`${BASE_URL}/transactions`);
+    const data = await response.json();
+    return data;
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+// Fetch transactions by ID
+export const fetchTransactionsById = async (id: number) => {
+  try {
+    const response = await fetch(`${BASE_URL}/transactions/${id}`);
+    const data = await response.json();
+    return data;
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+// Fetch User's transactions
+export const fetchTransactionsByUserId = async (userId: number) => {
+  try {
+    const response = await fetch(`${BASE_URL}/transactions/user/${userId}`);
+    const data = await response.json();
+    return data;
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+// Update transaction
+export const updateTransactions = async (
+  transactionId: number,
+  payload: ITransactions
+) => {
+  try {
+    const response = await fetch(`${BASE_URL}/transactions/${transactionId}`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(payload),
+    });
+    const data = await response.json();
+    return data;
+  } catch (e) {
+    console.log(e);
+  }
+};
