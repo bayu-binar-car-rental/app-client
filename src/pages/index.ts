269298aa8789b4ed805420a4965b1b3a93368b5a
TamaWira <asusrog20012001@gmail.com>
2024-05-15 17:20:43 +0800

    refactor: move user router index

--- src/pages/index.ts
+++
@@ -1,9 +0,0 @@
-export { default as UserLayout } from "../components/features/user/UserLayout";
-export { default as LandingPage } from "./user/LandingPage";
-export { default as CarSearchPage } from "./user/CarSearchPage";
-export { default as CarListPage } from "./user/CarListPage";
-export { default as CarDetailPage } from "./user/CarDetailPage";
-export { default as CarCheckoutPage } from "./user/CarCheckoutPage";
-export { default as CarPaymentPage } from "./user/PaymentPage";
-
-export { default as UserTransactionsPage } from "./user/profile/UserTransactionsPage";
